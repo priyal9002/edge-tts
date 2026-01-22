16ab119fb07acf53821296423f2813e0ae975a40
priyal9002 <85879562+priyal9002@users.noreply.github.com>
2026-01-22 10:25:14 +0000

    add new api

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const { Communicate } = require("edge-tts-universal");
+const { Communicate, } = require("edge-tts-universal");
 
 const app = express();
 app.use(express.json());
@@ -10,45 +10,56 @@ const TMP_DIR = path.join(__dirname, "tmp");
 if (!fs.existsSync(TMP_DIR)) fs.mkdirSync(TMP_DIR);
 
 app.post("/tts", async (req, res) => {
-  try {
-    const {
-      text,
-      voice = "en-US-AriaNeural",
-      rate = "0%",
-      pitch = "0%"
-    } = req.body;
+    try {
+        const {
+            text,
+            voice = "en-US-AriaNeural",
+            rate = "0%",
+            pitch = "0%"
+        } = req.body;
 
-    if (!text) {
-      return res.status(400).json({ error: "Text is required" });
+        if (!text) {
+            return res.status(400).json({ error: "Text is required" });
+        }
+
+        const fileName = `tts-${Date.now()}.mp3`;
+        const outputPath = path.join(TMP_DIR, fileName);
+
+        const tts = new Communicate(text, voice, {
+            rate,
+            pitch,
+            format: "audio-24khz-48kbitrate-mono-mp3"
+        });
+
+        await tts.save(outputPath);
+
+        res.setHeader("Content-Type", "audio/mpeg");
+        res.setHeader("Content-Disposition", `inline; filename="${fileName}"`);
+
+        const stream = fs.createReadStream(outputPath);
+        stream.pipe(res);
+
+        stream.on("close", () => {
+            fs.unlink(outputPath, () => { });
+        });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "TTS generation failed" });
     }
-
-    const fileName = `tts-${Date.now()}.mp3`;
-    const outputPath = path.join(TMP_DIR, fileName);
-
-    const tts = new Communicate(text, voice, {
-      rate,
-      pitch,
-      format: "audio-24khz-48kbitrate-mono-mp3"
-    });
-
-    await tts.save(outputPath);
-
-    res.setHeader("Content-Type", "audio/mpeg");
-    res.setHeader("Content-Disposition", `inline; filename="${fileName}"`);
-
-    const stream = fs.createReadStream(outputPath);
-    stream.pipe(res);
-
-    stream.on("close", () => {
-      fs.unlink(outputPath, () => {});
-    });
-
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "TTS generation failed" });
-  }
 });
 
+app.get("/check", async (req, res) => {
+    try {
+        const getAllVoicesList = await listVoices()
+
+        res.status(200).json(getAllVoicesList);
+    } catch (err) {
+        res.status(500).json({ error: "TTS generation failed" });
+        console.error(err);
+    }
+})
+
 app.listen(3000, () => {
-  console.log("🚀 Edge TTS API running on http://localhost:3000");
+    console.log("🚀 Edge TTS API running on http://localhost:3000");
 });
